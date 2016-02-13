67b4bfb0f662d8759bb87a58d5b01569ddf21f8b
anthonykoch <anthonykoch10@gmail.com>
2016-02-13 12:10:01 -0700

    Rename extensions to .jsx

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -49,7 +49,7 @@ var webpackConfig = {
 	{
 		noParse: [],
 		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+			{ test: /\.js|\.jsx$/, exclude: /node_modules/, loader: "babel-loader"}
 		],
 	},
 	plugins:
